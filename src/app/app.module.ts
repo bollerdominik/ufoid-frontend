519bdae8828f50d5d3d925ca3b8cb6df8c425349
bollerdominik <boller.dominik@gmail.com>
2017-05-27 14:35:21 +0100

    added video components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPostComponent } from './video-list/video-post/video-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    VideoDetailComponent,
+    VideoListComponent,
+    VideoPostComponent
   ],
   imports: [
     BrowserModule,
