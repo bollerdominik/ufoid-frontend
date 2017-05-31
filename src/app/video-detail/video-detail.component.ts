015b846738bae7776d2468be62b460976e080d7c
bollerdominik <boller.dominik@gmail.com>
2017-05-31 12:05:25 +0100

    started videodetail component

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {VideoPostService} from "../video-post.service";
+import {VideoPost} from "../video-list/video-post.model";
 
 @Component({
   selector: 'app-video-detail',
@@ -7,6 +8,7 @@ import {VideoPostService} from "../video-post.service";
   styleUrls: ['./video-detail.component.css']
 })
 export class VideoDetailComponent implements OnInit {
+  private videoPost: VideoPost;
 
   constructor(private videoPostService: VideoPostService) {
   }
@@ -15,8 +17,10 @@ export class VideoDetailComponent implements OnInit {
     if (!this.videoPostService.videoPost) {
       console.log("is undefined");
       // Load data from api
+      this.videoPost = new VideoPost("Another  video", "userABC", "London UK", 10);
     } else {
       console.log(this.videoPostService.videoPost);
+      this.videoPost = this.videoPostService.videoPost;
     }
   }
 
