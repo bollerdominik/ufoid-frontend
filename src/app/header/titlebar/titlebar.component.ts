035acccdac3079446f293cde3bdcd3b68e218f48
Dominik Boller <boller.dominik@gmail.com>
2017-11-06 13:18:53 +0100

    added sub title

--- src/app/header/titlebar/titlebar.component.ts
+++ src/app/header/titlebar/titlebar.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class TitlebarComponent implements OnInit {
 
   @Input() public title: string;
+  @Input() public subTitle: string;
 
   constructor() { }
 
